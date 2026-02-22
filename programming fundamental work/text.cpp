52931732ec87c20ae42ef0f4449d0e8b6ed062e9
Mahnoorfatima005 <fatimazafarswl@gmail.com>
2026-02-22 01:34:45 -0800

    add files to programming fundamentals

---
+++ programming fundamental work/text.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+main(){system("color 57");
+
+cout<<"  .....           ooooo   ooooo           .....             "<<endl; 
+cout<<"  ......         o     o  o   o          ......             "<<endl;
+cout<<"  .......           ooo   o   o         .......             "<<endl;
+cout<<"    ......         oo     o   o          .....              "<<endl;
+cout<<"      ....        oooooo  ooooo th       ....               "<<endl;
+cout<<"       ...       -------------------     ...                "<<endl;
+cout<<"        ..        C  E  N  T  U  R  Y    ..                 "<<endl;
+cout<<"         .       ---------------------    .                 "<<endl;
+cout<<"      __||__     ooooo oooo o  o ooooo  __||__              "<<endl;
+cout<<"     |      |      o   o     oo    o   |      |             "<<endl;
+cout<<"     |      |      o   oooo   o    o   |      |             "<<endl;
+cout<<"     |      |      o   o      oo   o   |      |             "<<endl;
+cout<<"_____|______|_____ o   oooo  o  o  o___|______|______        "<<endl;
+
+
+
+} 

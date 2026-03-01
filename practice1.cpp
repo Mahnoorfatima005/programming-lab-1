54c49e0c0aecbec18856d0598aa5d09941c229a1
Mahnoorfatima005 <fatimazafarswl@gmail.com>
2026-03-01 07:28:22 -0800

    Add files via upload
    add files to week 3 in programming-lab-1

---
+++ practice1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+main()
+{
+int sides;
+cout<<"Enter sides: ";
+cin>>sides;
+int angles = (sides-2)*180;
+cout<<"the sum of angles: ";
+cout<<"sum of angles == "<<angles;}
